d3519446e12aa17d5bdbb83abcefff76fdbd02db
janvikaliya25 <janvikaliya@gmail.com>
2025-03-25 18:16:15 +0530

    add

---
+++ Day-1/index.js
@@ -0,0 +1,20 @@
+const http = require("http")  // step 1
+const port=2345; // we are give any unique number 
+
+const serverHendler =(req,res)=>{
+    res.write(`<h1>Welcome to my server  ${port} ..</h1>`)
+    res.end()
+}
+
+const server=http.createServer(serverHendler) //step 2
+
+server.listen(port,(err)=>{
+    err?console.log("err"):console.log(`Your server is created ${port}`)    //step 3 
+
+    // if(err){
+    //     console.log("err")
+    // }
+    // else{
+    //     console.log(`welcome to my server port : ${port}`)
+    // }
+})
