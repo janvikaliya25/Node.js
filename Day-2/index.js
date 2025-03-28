51c4b6c6dc536185a21f7775a9179fa282bce07e
janvikaliya25 <janvikaliya@gmail.com>
2025-03-28 16:55:46 +0530

    add

--- Day-2/index.js
+++ Day-2/index.js
@@ -1,20 +1,24 @@
-const express=require("express");  //step-1
-const port=2506; // step-2
+let express=require("express");  // step-1
+let port=2506; // step -2
 
-const app=express();  // step-3
+let app=express();  // step-3
 
-app.set("view engine","ejs")  // step-6
+app.set("view engine","ejs");  // step-6
 
-let students=[  // step-7
-    {id:1,name:"janvi",subject:"Node.js"}
+let students=[   // step-7
+    {
+        id:1,
+        name:"janvi",
+        surname:"kaliya",
+        subject:"Node.js",
+        course:"Full-Stack Development"
+    }
 ]
-
 app.get("/",(req,res)=>{  // step-5
-    res.render("index",{students})
+    res.render("index",{students});
     res.end();
 })
 
-
 app.listen(port,(err)=>{  // step-4
-    err?console.log(err):console.log(`server is create ${port}`);
+    err?console.log(err):console.log(`your server is created ${port}`);
 })
