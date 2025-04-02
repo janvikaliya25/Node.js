042739470c4f8ca01e59ac896289cc7521785639
janvikaliya25 <janvikaliya@gmail.com>
2025-04-02 10:25:03 +0530

    add

--- Day-4/index.js
+++ Day-4/index.js
@@ -1,11 +1,12 @@
-let express=require("express");
-let port=2205;
+let express=require("express");  // step-1
+let port=2205;  // step-2
 
-let app=express();
+let app=express();  // step-3
 
-app.set("view engine","ejs")
+app.set("view engine","ejs")  // step-6
+app.use(express.urlencoded({extended:true}));  // step-9
 
-let students=[
+let students=[  // step-7
     {
         id:1,
         name:"Janvi",
@@ -14,11 +15,23 @@ let students=[
     }
 ]
 
-app.get("/",(req,res)=>{
+app.get("/",(req,res)=>{  // step-5
     res.render("index",{students});
     // res.end();
 })
 
-app.listen(port,(err)=>{
+app.post("/addData",(req,res)=>{  // step-8
+    console.log(req.body)
+    req.body.id=students.length+1  // step-10
+    students.push(req.body)
+    res.redirect("/")
+})
+
+app.post("/deleteData",(req,res)=>{
+    let newData=students.filter((item)=>item.id!=req.query.id)
+    students=newData;
+    res.redirect("/")
+})
+app.listen(port,(err)=>{  // step-4
     err?console.log(err):console.log(`Your server is created ${port}`)
 })
