0282fcae28cd6fc9dc2ff532cdfd941677cb41e6
janvikaliya25 <janvikaliya@gmail.com>
2025-04-03 11:57:54 +0530

    add

--- Day-4/index.js
+++ Day-4/index.js
@@ -24,13 +24,35 @@ app.post("/addData",(req,res)=>{  // step-8
     console.log(req.body)
     req.body.id=students.length+1  // step-10
     students.push(req.body)
-    res.redirect("/")
+    res.redirect("/") 
 })
 
-app.post("/deleteData",(req,res)=>{
-    let newData=students.filter((item)=>item.id!=req.query.id)
-    students=newData;
+app.get("/editData/:id",(req,res)=>{  // step-12
+    console.log(req.params)
+    let singleData=students.find((el)=> el.id == req.params.id)
+
+    res.render("edit",{singleData})
+})
+
+app.post("/updateData",(req,res)=>{  // step-13
+    console.log(req.body.id)
+    students.forEach((el)=>{
+        if(el.id == req.body.id){
+            el.name=req.body.name;
+            el.surname=req.body.surname;
+            el.subject=req.body.subject;
+        }
+        else{
+            el
+        }
+    });
     res.redirect("/")
+})
+app.get("/deleteData",(req,res)=>{  // step-11
+   console.log(req.query.id)
+   let newData=students.filter((el,i)=>el.id!=req.query.id)
+   students = newData;
+   res.redirect("/")
 })
 app.listen(port,(err)=>{  // step-4
     err?console.log(err):console.log(`Your server is created ${port}`)
