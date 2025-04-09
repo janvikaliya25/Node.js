135d779918d9b4f841d9d4aee08751b734fd7816
janvikaliya25 <janvikaliya@gmail.com>
2025-04-09 17:51:29 +0530

    add

--- Day-6/index.js
+++ Day-6/index.js
@@ -19,7 +19,7 @@ app.get("/",async(req,res)=>{ // 5
     //     res.redirect("index",{Student})
     // })
 })
-
+ 
 app.post("/addData",async(req,res)=>{ // 8
     let data= await schema.create(req.body); // 11
     data && res.redirect("/")
@@ -27,6 +27,26 @@ app.post("/addData",async(req,res)=>{ // 8
     // await schema.create(req.body).then(()=>{ // second method
     //     res.redirect("/")
     // })
+})
+
+app.get("/deleteData",async(req,res)=>{ // 12
+    console.log(req.query.id)
+    await schema.findByIdAndDelete(req.query.id).then(()=>{
+        res.redirect("/")
+    })
+})
+
+app.get("/editData",async(req,res)=>{ // 13
+    console.log(req.query.id)
+    await schema.findById(req.query.id).then((data)=>{
+        res.render("edit",{data})
+    })
+})
+
+app.post("/updateData",async(req,res)=>{ // 14
+    await schema.findByIdAndUpdate(req.body.id,req.body).then(()=>{
+        res.redirect("/")
+    })
 })
 
 app.listen(port,(err)=>{ // 4
